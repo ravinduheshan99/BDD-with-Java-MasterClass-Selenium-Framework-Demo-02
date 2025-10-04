de1e3af9b6ac12e7816947a9a6331df77dcb7a24
CORPNET\hravlk <ravindu.haputhanthri@ifs.com>
2025-10-04 19:59:54 +0530

    How to run Selenium Cucumber Scenarios parallely with TestNG runner annotations

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -2,8 +2,14 @@ package cucumberOptions;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true)
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
