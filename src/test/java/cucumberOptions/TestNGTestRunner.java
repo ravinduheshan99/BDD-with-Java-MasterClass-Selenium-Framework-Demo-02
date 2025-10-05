d30ba42cb56b441227613e98ec72b73e4f288b31
CORPNET\hravlk <ravindu.haputhanthri@ifs.com>
2025-10-05 12:51:26 +0530

    Generate Extent Spark html reports with in Cucumber Selenium framework

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, tags = "@PLaceOrderTest or @SearchTest",
-plugin = {"html:target/cucumber.html","json:target/cucumber.json"})
+plugin = {"html:target/cucumber.html","json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
     @Override
